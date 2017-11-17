689c0a577374e068a2b545cafcfcc85f619c98c0
ian-ramos <ramos.ianc@gmail.com>
2017-11-16 20:54:21 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,26 @@
 export default (state = [], action) => {
-  return state;
+  switch(action.type) {
+    case 'ADD_QUOTE':
+      return [...state, action.quote]
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId)
+    case 'UPVOTE_QUOTE':
+      const arr = [...state]
+      for (const el of arr) {
+        if (el.id === action.quoteId) {
+          ++el.votes
+        }
+      }
+      return arr
+    case 'DOWNVOTE_QUOTE':
+      const another = [...state]
+      for (const el of another) {
+        if (el.id === action.quoteId && el.votes > 0) {
+          --el.votes
+        }
+      }
+      return another
+    default:
+      return state;
+  }
 }
